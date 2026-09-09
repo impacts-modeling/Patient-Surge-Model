bfb69df995142a783134662baf8f04a825630d1b
José Cricelio <joscriml@gmail.com>
2026-09-09 15:59:15 -0600

    fix workers to 2

--- R/01_config.R
+++ R/01_config.R
@@ -5,7 +5,7 @@ options(shiny.maxRequestSize = 50 * 1024^2)
 options(dplyr.summarise.inform = FALSE)
 
 available_cores <- future::availableCores()
-workers <- max(1L, min(4L, available_cores))
+workers <- max(1L, min(2L, available_cores))
 
 if (workers > 1L) {
   future::plan(future::multisession, workers = workers)
